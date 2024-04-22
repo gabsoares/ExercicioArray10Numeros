5ae0754ea23da6934eb3abcf3453f7da6183791f
gabsoares <soaresgabriel68@gmail.com>
2024-04-22 14:41:39 -0300

    Add project files.

---
+++ ExercicioArray10Numeros/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] numeros = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Pares");
+for (int i = 0; i < 10; i++)
+{
+    if (numeros[i] % 2 == 0)
+    {
+        Console.Write(numeros[i] + " ");
+    }
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Ímpares");
+for (int i = 0; i < 10; i++)
+{
+    if (numeros[i] % 2 != 0)
+    {
+        Console.Write(numeros[i] + " ");
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Primeira posição: " + numeros[0]);
+Console.WriteLine("Última posição: " + numeros[9]);
